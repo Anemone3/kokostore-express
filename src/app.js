89edfd7137360d74edc0dc09680a45a7639e7b87
kokonut <castaneda.jair.13@gmail.com>
2024-10-06 03:38:32 -0500

    start deploy

--- src/app.js
+++ src/app.js
@@ -6,31 +6,31 @@ import CategoriaRoutes from './routes/categorias.routes.js'
 import { errorHandler } from './app/middlewares/errorHandler.js';
 import helmet from 'helmet'
 
-const app = express()
+const server = express()
 
 
-app.use(helmet());
-app.use(logger('dev'));
+server.use(helmet());
+server.use(logger('dev'));
 
-app.use(express.json());
-app.disable('x-powered-by'); //deshabilitar el header x-powered-by express
-app.use(errorHandler);
+server.use(express.json());
+server.disable('x-powered-by'); //deshabilitar el header x-powered-by express
+server.use(errorHandler);
 
-app.get('/',(req,res)=>{
+server.get('/',(req,res)=>{
     res.send('Hello world')
 })
 
 
 
 //Rutas
-app.use('/products',ProductRoutes);
-app.use('/categories',CategoriaRoutes)
+server.use('/products',ProductRoutes);
+server.use('/categories',CategoriaRoutes)
 
 
-app.use((req,res)=>{
+server.use((req,res)=>{
     res.status(404).send('Error 404, no hay contenido para este endpoint');
 })
 
-app.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
     console.log(`Server running on port http://localhost:${PORT}`);
 })
