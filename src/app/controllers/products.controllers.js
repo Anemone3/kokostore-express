980fb4b9a92dba2bea2c2b7e814e213aac3bdd5c
kokonut <castaneda.jair.13@gmail.com>
2024-11-13 19:39:50 -0500

    feat: update certificate

--- src/app/controllers/products.controllers.js
+++ src/app/controllers/products.controllers.js
@@ -128,7 +128,8 @@ const getProductPaginated = async (req, res) => {
     }
 
     const result = await ProductModel.getProductPaginated(limitNum, offset);
-
+    console.log(result);
+    
     res.status(200).json(result.rows);
   } catch (error) {
     console.error(error);
